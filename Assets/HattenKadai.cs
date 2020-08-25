887a13d444be5d5fcd7a98558e6224a51c264f8a
Yukina Arai <maloiq88@outlook.com>
2020-08-25 14:56:51 +0900

    Initial commit

---
+++ Assets/HattenKadai.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Boss
+{
+    private int mp = 53;
+
+    // 魔法攻撃用の関数
+    public void Magic()
+    {
+        if(mp >= 5)
+        {
+            mp -= 5;
+            Debug.Log("魔法攻撃をした。残りMPは" + mp + "。");
+        }
+        else
+        {
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+
+    }
+}
+
+    public class HattenKadai : MonoBehaviour
+{
+        // Start is called before the first frame update
+        void Start()
+        {
+            Boss boss = new Boss();
+
+            //Magic関数を10回使う
+            for(int i = 0; i < 11; i++)
+            {
+                //魔法用の関数を呼び出す  
+                boss.Magic();
+            }
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+    {
+        
+    }
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 要素の個数が5の、int型の配列arrayを宣言して好きな値で初期化
+        int[] array = { 10, 20, 30, 40, 50 };
+
+        //配列の各要素の値を順番に表示
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        //配列の各要素の値を逆順に表示
+        for (int i = 4; i > -1; i--)
+        {
+            Debug.Log(array[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
